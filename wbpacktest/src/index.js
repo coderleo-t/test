4b4f1078d4a07953399b80cdc708f059e81eacf8
leolee <q441987091@gmail.com>
2020-06-01 21:10:11 +0800

    finish base  webpack

--- wbpacktest/src/index.js
+++ wbpacktest/src/index.js
@@ -1,6 +1,9 @@
 import './assets/style/base.css'
+import {
+  Promise
+} from './a.js'
 function ab(a, b) {
   return a + b
 }
-
 console.log(ab(1, 2))
+console.log(new Promise(1, 2, 3))
--- wbpacktest/webpack.config.js
+++ wbpacktest/webpack.config.js
@@ -1,9 +1,13 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const postcssPresetEnv = require('postcss-preset-env')
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'build.js',
+    filename: 'js/build.js',
     path: path.resolve(__dirname, 'dist')
   },
   module: {
@@ -11,14 +15,51 @@ module.exports = {
       {
         test: /\.css$/,
         use: [
-          'style-loader',
-          'css-loader'
+          // 'style-loader',
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: () => [
+                postcssPresetEnv()
+              ]
+            }
+          }
         ]
+      },
+      {
+        exclude: /\.(js|html|css)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 8 * 1024,
+          outputPath: 'image',
+          esModule: false,
+          name: '[hash:10].[ext]'
+        }
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
       }
     ]
   },
   plugins: [
-
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'style/build.css'
+    }),
+    new OptimizeCssAssetsWebpackPlugin()
   ],
   mode: 'development'
+  // ,
+  // devServer: {
+  //   contentBase: path.resolve(__dirname, 'dist'),
+  //   compress: true,
+  //   port: 3306,
+  //   open: true
+  // }
 }
